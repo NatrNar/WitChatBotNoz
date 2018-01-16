bcb771c60f44a8f38842989d18bd08e66deaf627
NatrNar <Natr.Nar2@gmail.com>
2018-01-17 00:05:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/', function (req, res) {
 });
 
 // for facebook to verify
-app.get('/webhooks', function (req, res) {
+app.get('/webhook', function (req, res) {
     if (req.query['hub.verify_token'] === Config.FB_VERIFY_TOKEN) {
         res.send(req.query['hub.challenge']);
     }
@@ -33,23 +33,51 @@ app.get('/webhooks', function (req, res) {
 });
 
 // to send messages to facebook
-app.post('/webhooks', function (req, res) {
-    let entry = FB.getFirstMessagingEntry(req.body);
-    console.log("Yes");
-    // IS THE ENTRY A VALID MESSAGE?
-    if (entry && entry.message) {
-        if (entry.message.attachments) {
-            // NOT SMART ENOUGH FOR ATTACHMENTS YET
-            FB.fbMessage(entry.sender.id, "That's interesting!");
-        } else {
-            // SEND TO BOT FOR PROCESSING
-            Bot.client.message(entry.message.text).then((data) => {
-                FB.fbMessage(entry.sender.id,Bot.Answer(data));
+app.post('/webhook', function (req, res) {
+    /* let entry = FB.getFirstMessagingEntry(req.body);
+     // IS THE ENTRY A VALID MESSAGE?
+     if (entry && entry.message) {
+         if (entry.message.attachments) {
+             // NOT SMART ENOUGH FOR ATTACHMENTS YET
+             FB.fbMessage(entry.sender.id, "That's interesting!");
+         } else {
+             // SEND TO BOT FOR PROCESSING
+             Bot.client.message(entry.message.text).then((data) => {
+                 FB.fbMessage(entry.sender.id,Bot.Answer(data));
 
-            });
-        }
+             });
+         }
+     }
+
+     res.sendStatus(200)*/
+    // Parse the request body from the POST
+    let body = req.body;
+
+    // Check the webhook event is from a Page subscription
+    if (body.object === 'page') {
+
+        // Iterate over each entry - there may be multiple if batched
+        body.entry.forEach(function (entry) {
+
+            // Get the webhook event. entry.messaging is an array, but
+            // will only ever contain one event, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+        });
+
+        // Return a '200 OK' response to all events
+        res.status(200).send('EVENT_RECEIVED');
+
+    } else {
+        // Return a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
     }
 
-    res.sendStatus(200)
+
 });
 
