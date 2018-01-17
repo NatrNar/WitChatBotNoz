48835723cc74907a4e66b678c36daf18a97cfbd0
WitChatBotNoz <natr.nar2@gmail.com>
2018-01-17 03:26:04 +0200

    ok

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -9,7 +9,7 @@ var Bot = require('./bot');
 
 
 // LETS MAKE A SERVER!
-var app = express()
+var app = express();
 app.set('port', (process.env.PORT) || 5000);
 // SPIN UP SERVER
 app.listen(app.get('port'), function () {
@@ -55,58 +55,64 @@ app.get('/webhook', function (req, res) {
 
 // to send messages to facebook
 app.post('/webhook', function (req, res) {
-    let entry = FB.getFirstMessagingEntry(req.body)
+    let entry = FB.getFirstMessagingEntry(req.body);
     // IS THE ENTRY A VALID MESSAGE?
+    if (entry && entry.message) {
+        if (entry.message.attachments) {
+            // NOT SMART ENOUGH FOR ATTACHMENTS YET
+            FB.fbMessage(entry.sender.id, "That's interesting!");
+        }
+        else {
+
+            // Parse the request body from the POST
+            /*let body = req.body;
+            console.log(body);
+            // Check the webhook event is from a Page subscription
+            if (body.object === 'page') {
+
+                // Iterate over each entry - there may be multiple if batched
+                body.entry.forEach(function (entry) {
+
+                    // Get the webhook event. entry.messaging is an array, but
+                    // will only ever contain one event, so we get index 0
+                    let webhook_event = entry.messaging[0];
+                    console.log(webhook_event);
 
 
-    // Parse the request body from the POST
-    /*let body = req.body;
-    console.log(body);
-    // Check the webhook event is from a Page subscription
-    if (body.object === 'page') {
+                    // Get the sender PSID
+                    let sender_psid = webhook_event.sender.id;
+                    console.log('Sender PSID: ' + sender_psid);
 
-        // Iterate over each entry - there may be multiple if batched
-        body.entry.forEach(function (entry) {
+                    // pass the event to the appropriate handler function
+                    if (webhook_event.message) {
+                        handleMessage(sender_psid, webhook_event.message);
+                    }
 
-            // Get the webhook event. entry.messaging is an array, but
-            // will only ever contain one event, so we get index 0
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
+                });
 
+                // Return a '200 OK' response to all events
+                res.status(200).send('EVENT_RECEIVED');
 
-            // Get the sender PSID
-            let sender_psid = webhook_event.sender.id;
-            console.log('Sender PSID: ' + sender_psid);
-
-            // pass the event to the appropriate handler function
-            if (webhook_event.message) {
-                handleMessage(sender_psid, webhook_event.message);
+            } else {
+                // Return a '404 Not Found' if event is not from a page subscription
+                console.log("404");
+                res.sendStatus(404);
             }
+        */
 
-        });
+            // SEND TO BOT FOR PROCESSING
+            Bot.client.message(entry.message.text).then((data) => {
+                FB.fbMessage(entry.sender.id, Bot.Answer(data));
 
-        // Return a '200 OK' response to all events
-        res.status(200).send('EVENT_RECEIVED');
+            });
 
-    } else {
-        // Return a '404 Not Found' if event is not from a page subscription
-        console.log("404");
-        res.sendStatus(404);
+        }
+        res.sendStatus(200);
     }
-*/
-
-    // SEND TO BOT FOR PROCESSING
-    Bot.client.message(entry.message.text).then((data) => {
-        FB.fbMessage(entry.sender.id,Bot.Answer(data));
-
-    });
-
-
-    res.sendStatus(200)
 });
 
 
-function callSendAPI(sender_psid, response) {
+/*function callSendAPI(sender_psid, response) {
     // Construct the message body
     let request_body = {
         "recipient": {
@@ -128,9 +134,9 @@ function callSendAPI(sender_psid, response) {
             console.error("Unable to send message:" + err);
         }
     });
-}
+}*/
 
-function handleMessage(sender_psid, received_message) {
+/*function handleMessage(sender_psid, received_message) {
 
     let response;
 
@@ -145,4 +151,4 @@ function handleMessage(sender_psid, received_message) {
 
     // Sends the response message
     callSendAPI(sender_psid, response);
-}
+}*/
