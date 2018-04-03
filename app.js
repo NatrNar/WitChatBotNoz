4baee28bae5287079208ac51e0885a7812a41129
WitChatBotNoz <natr.nar2@gmail.com>
2018-04-03 23:11:52 +0300

    ok

--- app.js
+++ app.js
@@ -185,4 +185,17 @@ app.post('/Skype', connector.listen());
 
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("You said: %s", session.message.text);
+
+
+    bot.on('contactRelationUpdate', function (message) {
+        if (message.action === 'add') {
+            var name = message.user ? message.user.name : null;
+            var reply = new builder.Message()
+                .address(message.address)
+                .text("Hello %s... Thanks for adding me. Say 'hello' to see some great demos.", name || 'there');
+            bot.send(reply);
+        } else {
+            // delete their data
+        }
+    });
 });
